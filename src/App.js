7e08299806b01eb0eec065cdc424763ab8140501
Devichand1 <devichandinikhiya@gmail.com>
2022-04-24 19:25:25 +0530

    otp

--- src/App.js
+++ src/App.js
@@ -1,47 +1,84 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
 
-import './App.css';
-import { sendNotification } from './service/Notification';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
- const App=()=> { 
-      const [isLoactionLoaded, setisLoactionLoaded] = useState()
-  const handleNotification = () => {
-
-    Notification.requestPermission().then(permission => {
-      if (permission === 'granted') {
-        // new Notification('Hi there! Test Notification', {
-        //     "body": "Did you make a $1,000,000 purchase at Dr. Evil...",
-        //     "vibrate": [200, 100, 200, 100, 200, 100, 400],
-        //     "tag": "request",
-        //     "actions": [
-        //     { "action": "yes", "title": "Yes", "icon": "images/yes.png" },
-        //     { "action": "no", "title": "No", "icon": "images/no.png" }
-        //     ],
-        //   icon: 'https://cdn.iconscout.com/icon/free/png-256/notification-2-1175416.png'
-        // });
-        sendNotification("Did you make a $1,000,000 purchase at Dr. Evil..", "Test Notification")
-      }
+import "./App.css";
+import { sendNotification } from "./service/Notification";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+const App = () => {
+  const [isLoactionLoaded, setisLoactionLoaded] = useState();
+  const [otp, setotp] = useState()
+  useEffect(() => {
+    if ("OTPCredential" in window) {
+      const ac = new AbortController();
+    
+      navigator.credentials
+        .get({
+          otp: { transport: ["sms"] },
+          signal: ac.signal
+        })
+        .then((otp) => {
+          setotp(otp.code)
+          ac.abort();
+        })
+        .catch((err) => {
+          ac.abort();
+          console.log(err);
+        });
     }
-    )
+  }, [])
+  const handleNotification = () => {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        sendNotification(
+          "Did you make a $1,000,000 purchase at Dr. Evil..",
+          "Test Notification"
+        );
+      }
+    });
 
-    serviceWorkerRegistration.showNotification('Test Notification')
+    serviceWorkerRegistration.showNotification("Test Notification");
+  };
+
+  const handleGetLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (data) => {
+        console.log("loca", data);
+        setisLoactionLoaded(data);
+      },
+      (error) => {
+        console.log("error", error, error.message);
+      }
+    );
+  };
+
+  function locationError(error) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        return "User denied the request for geolocation.";
+      case error.POSITION_UNAVAILABLE:
+        return "Location information is currently unavailable.";
+      case error.TIMEOUT:
+        return "Request for user location timed out.";
+      case error.UNKNOWN_ERROR:
+        return "An unknown error occurred.";
+    }
   }
-
-const handleGetLocation=()=>{
-navigator.geolocation.getCurrentPosition((data) => {
-console.log("loca",data);
-setisLoactionLoaded(data)
-})
-}
   return (
     <div className="App">
-        <button className='btn' onClick={handleNotification} >Send Custom notification (PWA)</button>
-        <button className='btn' onClick={handleGetLocation} >Get Location</button>
-{
-  isLoactionLoaded ?<p>Your current location is (Latitude: {isLoactionLoaded.coords.latitude} ,Longitude: {isLoactionLoaded.coords.longitude} )</p>: null
-}
+      <button className="btn" onClick={handleNotification}>
+        Send Custom notification (PWA)
+      </button>
+      <button className="btn" onClick={handleGetLocation}>
+        Get Location
+      </button>
+      {isLoactionLoaded ? (
+        <p>
+          Your current location is (Latitude: {isLoactionLoaded.coords.latitude}{" "}
+          ,Longitude: {isLoactionLoaded.coords.longitude} )
+        </p>
+      ) : null}
+      <p>OTP is {otp}</p>
     </div>
   );
-}
+};
 
 export default App;
